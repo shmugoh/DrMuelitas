463d8e2ccd232a834e255f1e3501a91185a205d7
Juan <57115669+juanpisss@users.noreply.github.com>
2023-07-06 21:24:27 -0500

    Implement Name Storage

--- KnowMePage.js
+++ KnowMePage.js
@@ -1,8 +1,46 @@
-import { View, Text, Button, TextInput } from 'react-native'
+import React from 'react';
+import { View, Text, Button, TextInput, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './styles';
 
+const storeName = async (FirstName, LastName) => {
+    try {
+        await AsyncStorage.setItem('FirstName', FirstName);
+        await AsyncStorage.setItem('LastName', LastName);
+    } catch (e) {
+        Alert.alert("Error", e);
+    }
+}; 
+
+const readName = async () => {
+    try {
+        const FirstName = await AsyncStorage.getItem('FirstName');
+        const LastName = await AsyncStorage.getItem('LastName');
+        if (FirstName !== null) {
+            return [FirstName, LastName]
+        }
+    } catch (e) {
+        Alert.alert("Error", e);
+    }
+};
+
 export default function KnowMePage({navigation}) {
-    // TODO: Implement AsyncStorage
+
+    const [FirstName, onChangeFirstName] = React.useState();
+    const [LastName, onChangeLastName] = React.useState();
+
+    React.useEffect(() => {
+        (async () => {
+            const name = await readName();
+            onChangeFirstName(name[0]);
+            onChangeLastName(name[1]);
+        })();
+    
+        return () => {
+          // cleans up function once components are unmounted
+        };
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.HeadingText}>Conoceme</Text>
@@ -10,20 +48,28 @@ export default function KnowMePage({navigation}) {
             <TextInput
                 style={{height: 40}}
                 placeholder="Nombre"
+                autoComplete="given-name"
+                value={FirstName}
+                onChangeText={onChangeFirstName}
             />
             <TextInput
                 style={{height: 40}}
                 placeholder="Apellido"
+                autoComplete="family-name"
+                value={LastName}
+                onChangeText={onChangeLastName}
             />
             
-            <Button
-                title="Guardar"
-                onPress={() => navigation.navigate('MainScreen')}
-            />
-            <Button
-                title="Volver Atras"
-                onPress={() => navigation.navigate('MainScreen')}
-            />
+            <View style={{flexDirection: 'row'}}>
+                <Button
+                    title="Guardar"
+                    onPress={async () => {storeName(FirstName, LastName); navigation.navigate('MainScreen');}}
+                />
+                <Button
+                    title="Volver Atras"
+                    onPress={() => navigation.navigate('MainScreen')}
+                />
+            </View>
         </View>
     )
 }
