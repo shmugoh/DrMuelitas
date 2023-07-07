9f6afc134d9181705365f1cf2b568adb38b60472
Juan <57115669+juanpisss@users.noreply.github.com>
2023-07-07 13:49:09 -0500

    Implement Reminder Functionality

--- pages/RemindMePage.js
+++ pages/RemindMePage.js
@@ -6,87 +6,82 @@ import Checkbox from 'expo-checkbox';
 import styles from '../styles';
 
 export default function RemindMePage({ navigation }) {
-  const [isChecked, setChecked] = useState(false);
+  // Weekdays
+  const weekdays = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  const [checkedWeekdays, setCheckedWeekdays] = useState(Array(7).fill(false));
+  const handleCheckboxChange = (index) => {
+    const updatedCheckedWeekdays = [...checkedWeekdays];
+    updatedCheckedWeekdays[index] = !updatedCheckedWeekdays[index];
+    setCheckedWeekdays(updatedCheckedWeekdays);
+  };
 
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
+  // Reminders
+  const [reminders, setReminders] = useState([
+    { label: 'Recordatorio 1:', time: '00:00', isDatePickerVisible: false },
+    { label: 'Recordatorio 2:', time: '00:00', isDatePickerVisible: false },
+    { label: 'Recordatorio 3:', time: '00:00', isDatePickerVisible: false },
+  ]);
+  const showDatePicker = (index) => {
+    const updatedReminders = [...reminders];
+    updatedReminders[index].isDatePickerVisible = true;
+    setReminders(updatedReminders);
   };
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
+  const hideDatePicker = (index) => {
+    const updatedReminders = [...reminders];
+    updatedReminders[index].isDatePickerVisible = false;
+    setReminders(updatedReminders);
   };
-  const handleConfirm = (date) => {
-    console.warn('A date has been picked: ', date);
-    hideDatePicker();
+  const handleConfirm = (index, date) => {
+    // Handle the selected date/time for the specific reminder
+    // console.warn('A date has been picked: ', date);
+    const updatedReminders = [...reminders];
+    updatedReminders[index].time = formatTime(date);
+    setReminders(updatedReminders);
+    hideDatePicker(index);
+  };
+  const formatTime = (date) => {
+    // Format the date object to a string in the desired format
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.HeadingText}>Recuerdame</Text>
       <Text style={styles.SubHeadingText}>Defina cada cuanto quieres que te lo recuerde</Text>
+      {weekdays.map((day, index) => (
+        <View key={day} style={{ flexDirection: 'row', marginTop: 5 }}>
+          <Checkbox
+            value={checkedWeekdays[index]}
+            onValueChange={() => handleCheckboxChange(index)}
+            style={{ marginRight: 5 }}
+          />
+          <Text style={{ marginBottom: 4, fontSize: 18 }}>{day}</Text>
+        </View>
+      ))}
 
-      {/* TODO: Repetir el CheckBox-Dia por Bucle */}
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Lunes</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Martes</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Miercoles</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Jueves</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Viernes</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Sabado</Text>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Checkbox value={isChecked} onValueChange={setChecked}></Checkbox>
-        <Text>Domingo</Text>
-      </View>
+      {reminders.map((reminder, index) => (
+        <View key={index} style={{ flexDirection: 'row' }}>
+          <Text style={{ marginTop: 7, fontSize: 18 }}>{reminder.label}</Text>
+          <Button title={reminder.time} onPress={() => showDatePicker(index)} />
+          <DateTimePickerModal
+            isVisible={reminder.isDatePickerVisible}
+            mode="time"
+            display="inline"
+            onConfirm={(date) => handleConfirm(index, date)}
+            onCancel={() => hideDatePicker(index)}
+          />
+        </View>
+      ))}
 
-      <View style={{ flexDirection: 'row' }}>
-        <Text>Recordatorio 1: </Text>
-        <Button title="00:00" onPress={showDatePicker} />
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="time"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Text>Recordatorio 2: </Text>
-        <Button title="00:00" onPress={showDatePicker} />
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="time"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Text>Recordatorio 3: </Text>
-        <Button title="00:00" onPress={showDatePicker} />
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="time"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-      </View>
-
-      <Button title="Guardar" onPress={() => navigation.navigate('MainScreen')} />
+      <Button
+        title="Guardar"
+        onPress={() => {
+          navigation.navigate('MainScreen');
+          console.warn(reminders);
+        }}
+      />
       <Button title="Volver Atras" onPress={() => navigation.navigate('MainScreen')} />
     </View>
   );
